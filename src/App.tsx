0cf75b6d4bab25e47774964b68dcbb65606ac36a
sasaki404 <takaaki.sasaki2001@gmail.com>
2023-12-27 22:19:45 +0900

    Appbar change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import { Container, Box } from '@mui/material';
 import { TodoForm } from './TodoForm';
 import { TodoList } from './TodoList';
@@ -9,9 +12,14 @@ export const App = () => {
     const [typeList, setTypeList] = useState([]);
     return (
         <Box>
-            <Container maxWidth='xs'>
-                <h1>作業時間記録</h1>
-            </Container>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        作業時間記録
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div style={{ marginBottom: 15 }}></div>
             <TodoForm todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList} typeList={typeList} setTypeList={setTypeList} />
             <TodoList todoList={todoList} setTodoList={setTodoList} typeList={typeList} setTypeList={setTypeList} />
         </Box>
